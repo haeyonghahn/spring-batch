b4cdf0dbd44b280c7123a6b8c7910ac96b3abb12
한해용 <yong7317>
2021-05-13 17:40:02 +0900

    quartz 설정 및 테스트

---
+++ spring-batch01/src/main/java/com/douzone/batch/job/SampleJob.java
@@ -0,0 +1,13 @@
+package com.douzone.batch.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class SampleJob extends QuartzJobBean {
+
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		System.out.println("test job..........");
+	}
+}
