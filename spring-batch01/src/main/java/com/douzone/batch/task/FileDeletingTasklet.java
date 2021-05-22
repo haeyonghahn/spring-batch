c2b8965e67f253e1fd269960e2b7a35215f9ccaa
haeyong Hahn <yong80211@gmail.com>
2021-05-22 15:41:07 +0900

    크롤링, 배치, 스케줄 활용

--- spring-batch01/src/main/java/com/douzone/batch/task/FileDeletingTasklet.java
+++ spring-batch01/src/main/java/com/douzone/batch/task/FileDeletingTasklet.java
@@ -1,41 +1,30 @@
 package com.douzone.batch.task;
 
-import java.io.File;
+import java.util.Iterator;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.core.io.Resource;
 
 public class FileDeletingTasklet implements Tasklet {
-
-	private Resource directory;
-	
-	public Resource getDirectory() {
-		return directory;
-	}
-
-	public void setDirectory(Resource directory) {
-		this.directory = directory;
-	}
 	
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {		
+		String url = "http://www.cgv.co.kr/movies/";
+		Document doc = null;
+		
 		try {
-			File dir = directory.getFile();
-			File[] files = dir.listFiles();
-			if(files.length > 0) {				
-				for(File file : files) {
-					boolean deleted = file.delete();
-					if(!deleted)
-						throw new Exception("파일이 삭제되지 않았습니다. " + file.getPath());
-					else 
-						System.out.println("파일이 삭제되었습니다. " + file.getPath());
-				}
-			}
-			else {
-				System.err.println("삭제할 파일이 없습니다. ---------------------------");
+			doc = Jsoup.connect(url).get();
+			Elements elements = doc.select("div.sect-movie-chart");
+			Iterator<Element> rank = elements.select("strong.rank").iterator();
+			Iterator<Element> title = elements.select("strong.title").iterator();
+			while(rank.hasNext()) {
+				System.out.println(rank.next().text() + " " + title.next().text());
 			}
 		} catch(Exception e) {
 			throw e;
