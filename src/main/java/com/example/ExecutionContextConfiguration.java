d890277184a8e8fe2cde6cee7f2918a015e2a9e4
haeyonghahn <yong80211@gmail.com>
2023-03-06 07:39:44 +0900

    ExecutionContext

---
+++ src/main/java/com/example/ExecutionContextConfiguration.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class ExecutionContextConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    private final ExecutionContextTasklet1 executionContextTasklet1;
+
+    @Bean
+    public Job BatchJob() {
+        return this.jobBuilderFactory.get("Job")
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(executionContextTasklet1)
+                .build();
+    }
+
+}
