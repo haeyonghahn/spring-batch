370855016d35268c8c186052b652862b673644be
haeyonghahn <yong80211@gmail.com>
2023-04-25 17:21:07 +0900

    46.ItemStream

--- src/main/java/com/example/ItemStreamConfiguration.java
+++ src/main/java/com/example/ItemStreamConfiguration.java
@@ -5,25 +5,24 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class ItemReader_ItemProcessor_ItemWriter_Configuration {
+public class ItemStreamConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job BatchJob() {
-        return this.jobBuilderFactory.get("BatchJob")
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
                 .start(step1())
                 .next(step2())
                 .build();
@@ -32,25 +31,21 @@ public class ItemReader_ItemProcessor_ItemWriter_Configuration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(3)
+                .<String, String>chunk(5)
                 .reader(itemReader())
-                .processor(itemProcessor())
                 .writer(itemWriter())
                 .build();
     }
 
     @Bean
-    public ItemReader itemReader() {
-        return new CustomItemReader(Arrays.asList(
-                new Customer("user1"),
-                new Customer("user2"),
-                new Customer("user3")
-        ));
-    }
+    public CustomItemStreamReader itemReader() {
+        List<String> items = new ArrayList<>(10);
 
-    @Bean
-    public ItemProcessor itemProcessor() {
-        return new CustomItemProcessor();
+        for(int i = 1; i <= 10; i++) {
+            items.add(String.valueOf(i));
+        }
+
+        return new CustomItemStreamReader(items);
     }
 
     @Bean
@@ -61,10 +56,10 @@ public class ItemReader_ItemProcessor_ItemWriter_Configuration {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
+            .tasklet((contribution, chunkContext) -> {
+                System.out.println("step2 has executed");
+                return RepeatStatus.FINISHED;
+            })
+            .build();
     }
 }
