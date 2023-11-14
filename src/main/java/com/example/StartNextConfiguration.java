98807cc20c768ef30b843bf42e3039991c62dcef
haeyonghahn <yong80211@gmail.com>
2023-11-15 00:30:30 +0900

    16.SimpleJob_-_start()_/_next()

--- src/main/java/com/example/StartNextConfiguration.java
+++ src/main/java/com/example/StartNextConfiguration.java
@@ -1,15 +1,9 @@
 package com.example;
 
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
-public class SimpleJobConfiguration {
+public class StartNextConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
@@ -26,19 +20,12 @@ public class SimpleJobConfiguration {
 	@Bean
 	public Job batchJob() {
 		return this.jobBuilderFactory.get("batchJob")
-			.incrementer(new RunIdIncrementer())
-			.validator(new JobParametersValidator() {
-				@Override
-				public void validate(JobParameters parameters) throws JobParametersInvalidException {
-
-				}
-			})
-			.preventRestart()
 			.start(step1())
 			.next(step2())
 			.next(step3())
 			.build();
 	}
+
 	@Bean
 	public Step step1() {
 		return stepBuilderFactory.get("step1")
@@ -61,9 +48,6 @@ public class SimpleJobConfiguration {
 	public Step step3() {
 		return stepBuilderFactory.get("step3")
 			.tasklet((contribution, chunkContext) -> {
-				//                    throw new RuntimeException("failed");
-				chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-				contribution.setExitStatus(ExitStatus.STOPPED);
 				System.out.println("step3 has executed");
 				return RepeatStatus.FINISHED;
 			})
