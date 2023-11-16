bf839249c4247532421e1263542ff2c5143dc715
haeyonghahn <yong80211@gmail.com>
2023-11-16 23:13:29 +0900

    21.StepBuilderFactory

--- src/main/java/com/example/StepBuilderConfiguration.java
+++ src/main/java/com/example/StepBuilderConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
-public class SimpleJobArchitectureConfiguration {
+public class StepBuilderConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
@@ -24,7 +26,9 @@ public class SimpleJobArchitectureConfiguration {
 			.incrementer(new RunIdIncrementer())
 			.start(step1())
 			.next(step2())
-			.listener(new CustomJobListener())
+			.next(step4())
+			.next(step5())
+			//                .next(step3())
 			.build();
 	}
 
@@ -37,13 +41,51 @@ public class SimpleJobArchitectureConfiguration {
 			})
 			.build();
 	}
+
 	@Bean
 	public Step step2() {
 		return stepBuilderFactory.get("step2")
-			.tasklet((contribution, chunkContext) -> {
-				System.out.println("step2 has executed");
-				return RepeatStatus.FINISHED;
-			})
+			.<String, String>chunk(3)
+			.reader(() -> null)
+			.writer(list -> {})
 			.build();
+	}
+	/* @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .partitioner(step1())
+                .gridSize(2)
+                .build();
+    }
+	*/
+
+	@Bean
+	public Step step4() {
+		return stepBuilderFactory.get("step4")
+			.job(job())
+			.build();
+	}
+
+	@Bean
+	public Step step5() {
+		return stepBuilderFactory.get("step5")
+			.flow(flow())
+			.build();
+	}
+
+	@Bean
+	public Job job() {
+		return this.jobBuilderFactory.get("job")
+			.start(step1())
+			.next(step2())
+			//.next(step3())
+			.build();
+	}
+
+	@Bean
+	public Flow flow() {
+		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+		flowBuilder.start(step2()).end();
+		return flowBuilder.build();
 	}
 }
